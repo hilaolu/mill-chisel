c11bfe9316df61249bc346c765c5d2c9db223e93
hilaolu <i@phyzait.moe>
2021-12-08 16:02:58 +0000

    update

--- FSM/src/moore.scala
+++ FSM/src/moore.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 
 class MooreFSM extends MultiIOModule {
 
-  //checks seq 10011
+  //checks seq 10010
   //please pay attention to the bitorder
     val io = IO(
         new Bundle {
@@ -19,7 +19,7 @@ class MooreFSM extends MultiIOModule {
         }
     )
 
-    val sinit::s0::s1::s10::s100::s1001::s10011::Nil=util.Enum(7)
+    val sinit::s0::s1::s10::s100::s1001::s10010::Nil=util.Enum(7)
 
     val signal=io.in.signal.asBool()
     val next_state=Wire(UInt(3.W))
@@ -33,13 +33,13 @@ class MooreFSM extends MultiIOModule {
         s1       -> Mux(signal,s1,s10),
         s10      -> Mux(signal,s1,s100),
         s100     -> Mux(signal,s1001,s0),
-        s1001    -> Mux(signal,s10011,s10),
-        s10011   -> Mux(signal,s1,s10),
+        s1001    -> Mux(signal,s1,s10010),
+        s10010   -> Mux(signal,s1,s100),
     )
 
     next_state:=MuxLookup(current_state,sinit,state_matrix)
 
 
-    io.out.found:=current_state===s10011
+    io.out.found:=current_state===s10010
 
 }
