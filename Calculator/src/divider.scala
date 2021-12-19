0e3b2311b02ce5d12a800e3eddf14eb5426d9d5a
hilaolu <i@phyzait.moe>
2021-12-19 12:57:21 +0000

    update

---
+++ Calculator/src/divider.scala
@@ -0,0 +1,18 @@
+package Calculator
+
+import chisel3._
+import chisel3.util._
+
+class Divider extends MultiIOModule {
+
+    val io = IO(
+      new Protocol
+    )
+
+    io.out.result := io.in.op_1 / io.in.op_2
+    //todo use booth
+
+    io.out.end := true.B
+    io.out.busy := false.B
+
+}
